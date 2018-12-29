c5b9da9ff01dc7249b30cd55a7575149ac5fbcd5
Eddie Rusu <rusu24edward@gmail.com>
2018-12-28 20:19:23 -0800

    Changed all output in the Matrix test to output to the file

--- tutorial.cpp
+++ tutorial.cpp
@@ -6,7 +6,6 @@
 #include "Matrix.h"
 
 int TestMatrix();
-void Print(const Matrix&);
 void Print(const Matrix&, ofstream&);
 
 
@@ -36,40 +35,40 @@ int TestMatrix() {
 	try{
 
 		Matrix mat1;
-		Print(mat1);
+		Print(mat1, outFile);
 
 		Matrix mat2(2,2);
 		mat2.setName("Matrix 2");
-		Print(mat2);
+		Print(mat2, outFile);
 
 		Matrix mat3(mat1);
 		mat3.setName("Matrix 3");
-		Print(mat3);
+		Print(mat3, outFile);
 
 		Matrix mat4 = mat1;
 		mat4.setName("Matrix 4");
-		Print(mat4);
+		Print(mat4, outFile);
 
 		Matrix mat5(2,2,1.);
 		mat5.setName("Matrix 5");
-		Print(mat5);
+		Print(mat5, outFile);
 
 		Matrix mat6 = mat5;
 		mat6.setName("Matrix 6");
-		Print(mat6);
+		Print(mat6, outFile);
 
 		std::vector<std::vector<double>> vec1(9, vector<double>(2, -1.0));
 		Matrix mat7(vec1);
 		mat7.setName("Matrix 7");
-		Print(mat7);
+		Print(mat7, outFile);
 
 		Matrix mat8 = vec1;
 		mat8.setName("Matrix 8");
-		Print(mat8);
+		Print(mat8, outFile);
 
 		Matrix mat9(2,1);
 		mat9.setName("Matrix 9");
-		Print(mat9);
+		Print(mat9, outFile);
 
 		Matrix mat10(4,2);
 		mat10.setName("Matrix 10");
@@ -79,39 +78,39 @@ int TestMatrix() {
 		for (int i = 0; i < 4; ++i) {
 			mat10.insert(i,1,10-i);
 		}
-		Print(mat10);
+		Print(mat10, outFile);
 
 		Matrix mat11(1,1);
 		mat11.setName("Matrix 11");
 		mat11.insert(0,0,mat10.extract(1,1));
-		Print(mat11);
+		Print(mat11, outFile);
 
 		mat2 = vec1;
-		Print(mat2);
+		Print(mat2, outFile);
 
 		std::vector<std::vector<double>> vec2;
 		vec2.push_back(vector<double>(5));
 		vec2.push_back(vector<double>(3));
 		vec2.push_back(vector<double>(5));
-		std::cout << "\nAttempting to construct Matrix with inconsistent vector."
+		outFile << "\nAttempting to construct Matrix with inconsistent vector."
 				  << std::endl;
 		try {
 			Matrix mat12(vec2);
 			mat12.setName("Matrix 12");
 		} catch (const char* msg) {
-			std::cout << msg << std::endl;
+			outFile << msg << std::endl;
 		}
 
-		std::cout << "\nAttempting to assign Matrix to inconsistent vector."
+		outFile << "\nAttempting to assign Matrix to inconsistent vector."
 				  << std::endl;
 		try {
 			mat9 = vec2;
 		} catch (const char* msg) {
-			std::cout << msg << std::endl;
+			outFile << msg << std::endl;
 		}
-		Print(mat9);
+		Print(mat9, outFile);
 
-		std::cout << "\nAttempting to access out of range elements." << std::endl;
+		outFile << "\nAttempting to access out of range elements." << std::endl;
 		Matrix mat13(3,1);
 		mat13.setName("Matrix 13");
 		try {
@@ -119,27 +118,28 @@ int TestMatrix() {
 				mat13.insert(i,0,mat5.extract(i,1));
 			}
 		} catch (const char* msg) {
-			std::cout << msg << std::endl;
+			outFile << msg << std::endl;
 		}
 		try {
 			for (int i = 0; i < 3; ++i) {
 				mat13.insert(i,0,mat5(i,1));
 			}
 		} catch (const char* msg) {
-			std::cout << msg << std::endl;
+			outFile << msg << std::endl;
 		}
 		try {
 			for (int i = 0; i < 4; ++i) {
 				mat13.insert(i,0,mat5(0,1));
 			}
 		} catch (const char* msg) {
-			std::cout << msg << std::endl;
+			outFile << msg << std::endl;
 		}
-		Print(mat13);
+		Print(mat13, outFile);
 
 		status = 0;
 	} catch (...) {
 		std::cout << "Failed Matrix Test!" << std::endl;
+		outFile << "Failed Matrix Test!" << std::endl;
 		status = 1;
 	}
 
@@ -147,11 +147,6 @@ int TestMatrix() {
 	return status;
 }
 
-void Print(const Matrix& m) {
-	std::cout << m << std::endl;
-}
-
 void Print(const Matrix& m, ofstream& outFile) {
-std::cout << "In outFile streamer function" << std::endl;
 	outFile << m << std::endl;
 }
