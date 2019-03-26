ed3eada348133d8b3806453199ddaece430a085b
Eddie Rusu <rusu24edward@gmail.com>
2019-03-25 21:23:16 -0700

    Updated and tested Reader

--- Reader.cpp
+++ Reader.cpp
@@ -2,21 +2,34 @@
 #include "Reader.h"
 
 #include <vector>
+#include <sstream>
 
+// Read a comma delimitted file into a Matrix
+// @param const string& fileName - the name of the file
+// @return Matrix - the resulting Matrix
 Matrix Reader::Read(const string& fileName) {
 
 	ifstream inFile(fileName);
 	if (!inFile.is_open()) {
 		throw "ERROR:  "
 			  "Matrix Reader::Read(const string&)\n"
-			  "\tCannot open " << fileName << "!" << endl;
-		cout << "FAILURE"
+			  "\tCannot open file!";
 	}
 
 	vector<vector<double>> dataIn;
-	// Loop over all lines in the inFile with getline
-	// In each line, loop over all strings with stringstreamer
-	// rowIn.push_back(convert2Double(string));
+	string line;
+	while (getline(inFile, line)) { // Loop over each line
+		istringstream iss(line);
+		vector<double> rowIn;
+		double inputDouble;
+		while(iss >> inputDouble) { // Loop over each item in the line
+			rowIn.push_back(inputDouble);
+	        if (iss.peek() == ',') {
+	            iss.ignore();
+	        }
+		}
+		dataIn.push_back(rowIn);
+	}
 
 	inFile.close();
 
